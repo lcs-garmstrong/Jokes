383d4f90559d52f77998d7df53af2f054d04c4e6
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-04-14 14:23:29 -0400

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Graeme Armstrong on 2023-04-14.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
