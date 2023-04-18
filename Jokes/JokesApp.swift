87a3c8aa4eea2f0d399b66bcd38b6cfaff4c0d90
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-04-18 09:11:59 -0400

    Invoking helper code that copies the database into the app's sandbox and establishes a connection to the database.

--- Jokes/JokesApp.swift
+++ Jokes/JokesApp.swift
@@ -5,6 +5,7 @@
 //  Created by Graeme Armstrong on 2023-04-14.
 //
 
+import Blackbird
 import SwiftUI
 
 @main
@@ -12,6 +13,8 @@ struct JokesApp: App {
     var body: some Scene {
         WindowGroup {
             JokesView()
+            // make the database available within our app
+                .environment(\.blackbirdDatabase, AppDatabase.instance)
         }
     }
 }
